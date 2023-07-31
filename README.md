80805054fc10b56428eaefe826aca2a0c7908804
Murat ULUTÜRK <ctf.murat.uluturk@gmail.com>
2023-07-31 12:42:20 +0300

    Create main loop. Print out basic help messages.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## Description
 
-This is a pokedex app that allows you to search for pokemon and view their details. It also allows you to add pokemon to your favorites list. This program was built using golang and cli.
+This is a pokedex app that allows you to search for pokemon and view their details. It also allows you to add pokemon to your favorites list. This program is a Go REPL.
--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Buidl a REPL
+	fmt.Println(">> Welcome to the Pokedex REPL!")
+	fmt.Println(">> Type 'help' for instructions or 'exit' to quit.")
+	for {
+		fmt.Printf(">> ")
+		var input string
+		fmt.Scanln(&input)
+		switch input {
+		case "exit":
+			fmt.Println("Bye!")
+			return
+		case "help":
+			fmt.Println("You can use the program by typing commands when prompted.")
+			fmt.Println("The commands are:")
+			fmt.Println("  help   - show this help message")
+			fmt.Println("  exit   - exit the program")
+		default:
+			fmt.Println("Unknown command")
+		}
+	}
 }
